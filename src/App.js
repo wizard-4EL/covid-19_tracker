d3fa73443dced45333e70da05ccfc5466ee89ded
klenamm <harport361@gmail.com>
2025-02-16 14:34:13 -1200

    API Request

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { FormControl, Select, MenuItem } from "@mui/material";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country, // e.g., United States, United Kingdom
+            value: country.countryInfo.iso2, // e.g., USA, UK, FR
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
   return (
     <div className="app">
-       <h2>Let's build </h2>
+      <div className="app-header">
+        <h2>COVID-19 TRACKER</h2>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="worldwide">
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map((country) => (
+              <MenuItem key={country.value} value={country.value}>
+                {country.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
